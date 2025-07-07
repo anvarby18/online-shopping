ab285db8a9618bd3be13049a3804e7f52559e0f5
anvarby18 <anvarby18@gmail.com>
2025-07-07 21:28:44 +0530

    first controller class

---
+++ src/main/java/src/com/online/shopping/controller/Application.java
@@ -0,0 +1,8 @@
+package src.com.online.shopping.controller;
+
+public class Application {
+
+    public static void main(String[] args){
+        System.out.println("well-come to docker corrce");
+    }
+}
